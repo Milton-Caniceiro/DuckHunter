c4fc4fb19482caf41f9ab21597d0fa5814f7950a
Milton Caniceiro <122386359+Milton-Caniceiro@users.noreply.github.com>
2023-04-23 10:56:39 +0100

    game over still not working

--- custom.js
+++ custom.js
@@ -9,42 +9,20 @@ const playButton = document.getElementById('play-button');
 
 function startGame(){
 runningDog();
-setTimeout(jumpingDog, 5000);
-setTimeout(removeDog, 5000);
-if(bullet<0){
-    document.getElementById("gameOver").style.display = 'block';
-}
+setTimeout(jumpingDog, 6600);
+setTimeout(removeDog, 6500);
+shot();
 }
 
+function shot(){
 let bullet = 5;
 
 const shotSound = document.getElementById("shotSound");
-
-const decreaseBullets = () => {
-    const bullet = document.querySelector("#bullet-counter").innerHTML;
-
-    const bulletHTML = document.querySelector("#bullet-counter");
-
-    let count = Number(bullet);
-
-    bulletHTML.innerHTML = count - 1;
-};
-
-const increaseScore = () => {
-    const score = document.querySelector("#score-counter").innerHTML;
-
-    const scoreHTML = document.querySelector("#score-counter");
-
-    let count = Number(score);
-
-    scoreHTML.innerHTML = count + 100;
-};
 
 const duck = document.querySelector('#duck-image');
 
 duck.addEventListener('click', () => {
     shotSound.play();
-    fallDown();
     setTimeout(dogWithDuck, 3050)
     setTimeout(hiddeDogWithDuck, 6100);
     increaseScore();
@@ -81,47 +59,28 @@ const hiddeDogWithDuck = () => {document.getElementById('dogWithDuck');
     dogWithDuck.style.display = 'none';
 }
 
-function fallDown() {
-    const duckImage = document.getElementById("duck-image");
-    const deadDuck = document.getElementById("dead-duck");
-    const fallingDuck = document.getElementById("falling-duck");
-    deadDuck.style.top = `${parseInt(duckImage.style.top)}px`;
-    deadDuck.style.left = `${parseInt(duckImage.style.left)}px`;
-    fallingDuck.style.top = `${parseInt(duckImage.style.top)}px`;
-    fallingDuck.style.left = `${parseInt(duckImage.style.left)}px`;
-    duckImage.style.display = "none";
-    deadDuck.style.display = "block";
-    setTimeout(hideDeadDuck, 200);
-    setTimeout(showFallingDuck, 200);
-    
-    let positionY = parseInt(duckImage.style.top);
-    const interval = 50;
-    const diffY = 11;
-    let tID = setInterval(() => {
-        positionY += diffY;
-        const fieldHeight = document.getElementById('field').offsetHeight;
-        const spriteHeight = fallingDuck.offsetHeight;
-        const maxPositionY = fieldHeight - spriteHeight;
-        if (positionY > maxPositionY) {
-            clearInterval(tID);
-        }
-        fallingDuck.style.top = `${positionY}px`;
-    }, interval);
-    setTimeout(hideFallingDuck, 3000);
-
-
-const showFallingDuck = () => {
-    const fallingDuck = document.getElementById('falling-duck');
-    fallingDuck.style.display = 'block';
-  };
-
-const hideDeadDuck = () => {
-    const deadDuck = document.getElementById('dead-duck');
-    deadDuck.style.display = 'none';
-};
-
-const hideFallingDuck = () => {
-    const fallingDuck = document.getElementById('falling-duck');
-    fallingDuck.style.display = 'none';
-};
+if(bullet < 0){
+    document.getElementById("gameOver").style.display = 'block';
 }
+}
+
+const decreaseBullets = () => {
+    
+    const bullet = document.querySelector("#bullet-counter").innerHTML;
+
+    const bulletHTML = document.querySelector("#bullet-counter");
+
+    let count = Number(bullet);
+
+    bulletHTML.innerHTML = count - 1;
+};
+
+const increaseScore = () => {
+    const score = document.querySelector("#score-counter").innerHTML;
+
+    const scoreHTML = document.querySelector("#score-counter");
+
+    let count = Number(score);
+
+    scoreHTML.innerHTML = count + 100;
+};
